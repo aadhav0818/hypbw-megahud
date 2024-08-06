fe1e3fcabd27afc50428bb228f6a8e4af15ddf2f
Aadhav Ilangovan <aadhav0818@gmail.com>
2024-08-06 02:29:25 -0500

    added example auth file

---
+++ src/main/java/com/mod/hypixelmegahud/tokens/exampleApiAuth.java
@@ -0,0 +1,12 @@
+package com.mod.hypixelmegahud.tokens;
+
+public class exampleApiAuth {
+
+    private final String hypixelAPIKey = "<INSERT API KEY HERE>";
+
+    public exampleApiAuth() {}
+
+    public String getKey() {
+        return hypixelAPIKey;
+    }
+}
