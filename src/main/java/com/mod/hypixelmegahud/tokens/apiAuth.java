abe58cf7bac70b2bd962671334f225ba51b94d9c
Aadhav Ilangovan <78950532+aadhav0818@users.noreply.github.com>
2024-08-06 02:15:24 -0500

    Update apiAuth.java

--- src/main/java/com/mod/hypixelmegahud/tokens/apiAuth.java
+++ src/main/java/com/mod/hypixelmegahud/tokens/apiAuth.java
@@ -2,7 +2,7 @@ package com.mod.hypixelmegahud.tokens;
 
 public class apiAuth {
 
-    private final String hypixelAPIKey = "f8fd15bb-9838-4859-849d-bc7272f788ed";
+    private final String hypixelAPIKey = "<YOUR API KEY HERE>";
 
     public apiAuth() {}
 
